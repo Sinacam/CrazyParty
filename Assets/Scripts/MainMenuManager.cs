cc4238d178d34806f7581c800bb595d95051c667
ryan070111 <ryan070111@gmail.com>
2018-12-02 16:34:50 +0800

    commit

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -7,11 +7,7 @@ public class MainMenuManager : MonoBehaviour
 {
     void Update()
     {
-<<<<<<< HEAD
         if (Input.GetKeyDown(KeyCode.Space))
-=======
-        if (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
->>>>>>> master
             SceneManager.LoadScene("Lobby");
 		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
 			SceneManager.LoadScene("Lobby");
