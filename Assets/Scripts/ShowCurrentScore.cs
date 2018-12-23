79ad4578aa38657d1051801087638396540d3e17
nicole011635 <nicole2092455@gmail.com>
2018-12-23 19:42:28 +0800

    finish current result

--- Assets/Scripts/ShowCurrentScore.cs
+++ Assets/Scripts/ShowCurrentScore.cs
@@ -10,14 +10,29 @@ public class ShowCurrentScore : PlayerBehaviour {
     // Use this for initialization
     void Start () {
 
-        if (isLocalPlayer)
-            return;
+        //if (isLocalPlayer)
+            //return;
 
-        Text currentScore = gameObject.GetComponentInChildren<Text>();
-        //currentScore.text = "123";
-        currentScore.text = (goodScore + evilScore).ToString();
+        //Text currentScore = GameObject.Find("score").GetComponentInChildren<Text>();
 
-        print(goodScore);
+        //currentScore.text = (Persist.goodScores[0] + Persist.evilScores[0]).ToString();
+
+       // print(Persist.goodScores[0]);
+
+
+        GameObject[] scores = GameObject.FindGameObjectsWithTag("score");
+
+        int i = 0;
+        foreach (GameObject s in scores)
+        {
+            s.GetComponent<Text>().text = (Persist.goodScores[i] + Persist.evilScores[i]).ToString();
+            i++;
+        }
+        print(Persist.goodScores[0] + Persist.evilScores[0]);
+        print(Persist.goodScores[1] + Persist.evilScores[1]);
+        print(Persist.goodScores[2] + Persist.evilScores[2]);
+        print(Persist.goodScores[3] + Persist.evilScores[3]);
+
     }
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/ShowReslut.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowReslut : PlayerBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+
+        GameObject[] scores = GameObject.FindGameObjectsWithTag("score");
+
+        int i = 0;
+        foreach (GameObject s in scores)
+        {
+            s.GetComponent<RectTransform>().sizeDelta = new Vector2(Persist.goodScores[i] * 8, 70);
+            //s.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(Persist.evilScores[i] * 8, 70);
+            i++;
+        }
+        print(Persist.goodScores[0]);
+        print(Persist.goodScores[1]);
+        print(Persist.goodScores[2]);
+        print(Persist.goodScores[3]);
+
+        
+        //evil的都不動~~~
+        GameObject[] badscores = GameObject.FindGameObjectsWithTag("badscore");
+
+        int j = 0;
+        foreach (GameObject b in badscores)
+        {
+            b.GetComponent<RectTransform>().sizeDelta = new Vector2(Persist.evilScores[i] * 8, 70);
+            j++;
+        }
+        
+        print(Persist.evilScores[0]);
+        print(Persist.evilScores[1]);
+        print(Persist.evilScores[2]);
+        print(Persist.evilScores[3]);
+        
+
+    }
+	
+}
