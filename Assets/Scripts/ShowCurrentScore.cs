677fdb75cb1653a32beecbd0a927e499cc068f61
nicole011635 <nicole2092455@gmail.com>
2018-12-23 14:42:13 +0800

    try to add loadingnext show current score

---
+++ Assets/Scripts/ShowCurrentScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ShowCurrentScore : PlayerBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        if (isLocalPlayer)
+            return;
+
+        Text currentScore = gameObject.GetComponentInChildren<Text>();
+        //currentScore.text = "123";
+        currentScore.text = (goodScore + evilScore).ToString();
+
+        print(goodScore);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+ 
+    }
+
+}
