262fd176065a5ef2a61ae60741e530e8e8cbd760
nicole011635 <nicole2092455@gmail.com>
2018-12-25 16:38:38 +0800

    recover loading next bug:client 不會同步顯示分數

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ScoreManager : MonoBehaviour {
+    public string[] playerNames;//玩家名稱
+    public int[] playerScore;//玩家分數
+	// Use this for initialization
+	void Start () {
+        for(int i =0;i<playerNames.Length;i++)//在LevelTemplate.unity初始化
+        {
+            if (playerNames[i] == "")
+            {
+                PlayerPrefs.SetInt(("playerScore" + (i + 1).ToString()),0);
+            }
+            else
+            {
+                PlayerPrefs.SetInt(playerNames[i], 0);
+            }
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
