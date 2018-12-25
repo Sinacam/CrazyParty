3140896efe80f138f66a778fefbf38be6efc462f
nicole011635 <nicole2092455@gmail.com>
2018-12-25 17:52:53 +0800

    add finalresult ui

---
+++ Assets/Scripts/FinalResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalResult : MonoBehaviour {
+	
+	void Start () {
+		
+		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			SceneManager.LoadScene("Lobby");
+		}
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+			SceneManager.LoadScene("Lobby");
+		}
+	}
+}
