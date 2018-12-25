f650c7731467fb151cec5ce9e9d6f396ed4ee4b8
Sinacam <stinkingmadgod@gmail.com>
2018-12-26 01:03:14 +0800

    Fixed LoadingNext score text

--- Assets/Scripts/Framework/LoadingNext.cs
+++ Assets/Scripts/Framework/LoadingNext.cs
@@ -6,24 +6,9 @@ using UnityEngine.Networking;
 
 public class LoadingNext : NetworkBehaviour
 {
-    public Text t;
     void Start()
     {
         StartCoroutine(NextLevelIn(1));
-        /*
-        Debug.Log(Persist.goodScores[0]);
-        Debug.Log(Persist.goodScores[1]);
-        Debug.Log(Persist.goodScores[2]);
-        Debug.Log(Persist.goodScores[3]);*/
-        t = GameObject.Find("man_head").GetComponentInChildren<Text>();
-        t.text = Persist.goodScores[0].ToString();
-        t = GameObject.Find("elf_head").GetComponentInChildren<Text>();
-        t.text = Persist.goodScores[1].ToString();
-        t = GameObject.Find("girl_head").GetComponentInChildren<Text>();
-        t.text = Persist.goodScores[2].ToString();
-        t = GameObject.Find("krampus_head").GetComponentInChildren<Text>();
-        t.text = Persist.goodScores[3].ToString();
-
     }
 
     IEnumerator NextLevelIn(float t)
---
+++ Assets/Scripts/Framework/ScoreText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour {
+
+    bool rendered = false;
+	void Update ()
+    {
+        if (rendered)
+            return;
+
+        GameObject.Find("man_head").GetComponentInChildren<Text>().text = Persist.goodScores[0].ToString();
+        GameObject.Find("elf_head").GetComponentInChildren<Text>().text = Persist.goodScores[1].ToString();
+        GameObject.Find("girl_head").GetComponentInChildren<Text>().text = Persist.goodScores[2].ToString();
+        GameObject.Find("krampus_head").GetComponentInChildren<Text>().text = Persist.goodScores[3].ToString();
+
+        rendered = true;
+    }
+}
